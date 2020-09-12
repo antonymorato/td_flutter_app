1fff66f21028833696eb598f3a333dba41d903be
antonymorato <antonymorato2@gmail.com>
2020-09-12 22:41:53 +0300

    added refresh method to appstart utils, refactored isfirsttime method

--- lib/utils/app_start_utils.dart
+++ lib/utils/app_start_utils.dart
@@ -3,15 +3,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AppStartUtils{
 
+  static Future<void> refreshFirstTimeStart() async{
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('first_time', true);
+  }
+
   static Future<bool> isFirstTime() async {
     final prefs = await SharedPreferences.getInstance();  
-     var isFirstTime = prefs.getBool('first_time');
-     if (isFirstTime != null && !isFirstTime) {
-       prefs.setBool('first_time', false);
-       return false;
-     } else {
-       prefs.setBool('first_time', false);
-       return true;
-     }
+    var isFirstTime = prefs.getBool('first_time');
+    if (isFirstTime != null) 
+      return isFirstTime;
+    return false;
   }
 }
