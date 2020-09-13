83fcb1d7f1f1230a91a94cc8cf04d69988c5474f
antonymorato <antonymorato2@gmail.com>
2020-09-13 21:07:00 +0300

    added OS check for diff emulators localhost requests

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,17 +1,26 @@
 import 'dart:convert';
+import 'dart:io' show Platform;
 
 import 'package:http/http.dart' as http;
-class ServerApi{
 
-  static Future<bool> getReq() async{
+class ServerApi {
+  static Future<bool> getReq() async {
+    var osUrl="localhost";
+    if (Platform.isAndroid) {
+      osUrl="10.0.2.2";
+    } else if (Platform.isIOS) {
+      osUrl="127.0.0.1";
+    }
 
-    var url="http://10.0.2.2:5000/status";
-    http.Response response= await http.Client().get(url,headers: {
+    var url = "http://$osUrl:5000/status";
+    http.Response response = await http.Client().get(
+      url,
+      headers: {
         'Content-Type': 'application/json',
-      },);
+      },
+    );
     var res;
-    if(response.statusCode==200)
-      res=json.decode(response.body)['status'];
+    if (response.statusCode == 200) res = json.decode(response.body)['status'];
 
     print("Server:$res");
     return res;
